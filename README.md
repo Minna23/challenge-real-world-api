8af80876928eacbaeb8355c649e24f9aacc0f74a
Minna23 <andrea.teiermayer@gmail.com>
2023-03-19 16:40:03 +0000

    updated javascript, css, changed HTML, updated README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # challenge-real-world-api
+
+This challenge was part of my Front-End Development Bootcamp.
+
+The instructions of the challenge are below:
+When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history.
+
+### When a user views the current weather conditions for that city they are presented with:
+
+- The city name
+
+- The date
+
+- An icon representation of weather conditions
+
+- The temperature
+
+- The humidity
+
+- The wind speed
+
+### When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
+
+- The date
+
+- An icon representation of weather conditions
+
+- The temperature
+
+- The humidity
+
+### When a user clicks on a city in the search history they are again presented with current and future conditions for that city.
+
+As a resource for my javaScript I followed Drew's speedrun.
+
+I used HTML, CSS and JavaScript to solve this challenge.
+
+Image of deployed application:
+
+Link to deployed application:
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,143 @@
-
+// to get the javaScript code I followed pretty much Drew's thinking and method presented at his speedrun
 const weatherAPIURL = "https://api.openweathermap.org"
 const weatherAPIKey = ""
 let searchHistory = []
 let searchInput = $("#search-input");
 let searchForm = $("#search-form");
-let searchHistoryContainer = $("#history")
+let searchHistoryContainer = $("#history");
+let forecastContainer = $("#forecast");
+let todayWeatherContainer = $("#today")
 
-function renderSearchHistory() {
+function collectSearchHistory() {
     searchHistoryContainer.html("")
-            for (let i=0; i<searchHistory.length; i++){
-            let btn = $("<button>");
-            btn.attr("type", "submit")
-            btn.addClass("btn btn-primary history-btn btn-history")
-            btn.attr("data-search", searchHistory[i])
-            btn.text(searchHistory[i])
-            searchHistoryContainer.append(btn)
-            }
+        for (let i=0; i<searchHistory.length; i++){
+        let btn = $("<button>");
+        btn.attr("type", "submit")
+        btn.addClass("btn btn-primary history-btn btn-history")
+        btn.attr("data-search", searchHistory[i])
+        btn.text(searchHistory[i])
+        searchHistoryContainer.append(btn)
+        }
+}
+
+function appendSearchHistory(search) {
+    if (searchHistory.indexOf(search) !==-1) {
+        return;
+    }
+    searchHistory.push(search);
+    localStorage.setItem("search-history", JSON.stringify(searchHistory))
+    collectSearchHistory()
+}
+ // resource for this function: Drew's speedrun
+function renderCurrentWeather(city, weatherData){
+    let date = moment().format("DD, MMM, YYYY");
+    let tempC = weatherData["main"]["temp"];
+    let windKph = weatherData["wind"]["speed"];
+    let humidity = weatherData["main"]["humidity"];
+    let iconURL = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    let iconDescription = weatherData.weather[0].description || weatherData[0].main;
+    let heading = $("<h2>");
+    heading.attr("class", "h3-title")
+    let card = $("<div>");
+    card.attr("class", "card")
+    let cardBody = $("<div>");
+    cardBody.attr("class", "card-body")
+    card.append(cardBody)
+    let weatherIcon = $("<img>")
+
+    let tempEl = $("<p>");
+    tempEl.attr("class", "p-text")
+    let windEl = $("<p>");
+    windEl.attr("class", "p-text")
+    let humidityEl = $("<p>");
+    humidityEl.attr("class", "p-text")
+    heading.text(`${city} (${date})`)
+    weatherIcon.attr("src", iconURL)
+    weatherIcon.attr("alt", iconDescription)
+    heading.append(weatherIcon);
+    tempEl.text(`🌡️ ${tempC} ℃`);
+    windEl.text(`🌬️ ${windKph} Kph`);
+    humidityEl.text(`💦 ${humidity} %`);
+    cardBody.append(heading, tempEl, windEl, humidityEl);
+    todayWeatherContainer.html("");
+    todayWeatherContainer.append(card);
+
+}
+
+function renderForecast(weatherData) {
+    let headingColumn = $("<div>");
+    let heading = $("<h4>");
+    headingColumn.attr("class", "col-12");
+    heading.text("5-day Forecast");
+    headingColumn.append(heading);
+
+    forecastContainer.html("");
+    forecastContainer.append(headingColumn)
+    let futureForecast = weatherData.filter(function(forecast) {
+        return forecast.dt_txt.includes("12")
+        })
+    for (let i=0; i<futureForecast.length; i++) {
+        let iconURL = `https://openweathermap.org/img/w/${futureForecast[i].weather[0].icon}.png`
+        let iconDescription = futureForecast[i].weather[0].description;
+        let tempC = futureForecast[i].main.temp;
+        let windKph = futureForecast[i].wind.speed;
+        let humidity = futureForecast[i].main.humidity;
+        
+        
+
+        let col = $("<div>");
+        col.attr("class", "col-md")
+        let card = $("<div>");
+        card.attr("class", "card bg-primary h-100 text-white")
+        let cardBody = $("<div>");
+        cardBody.attr("class", "card-body")
+        let title = $("<h5>");
+        title.attr("card-title")
+        title.text(moment(futureForecast[i].dt_txt).format("DD/MM/YYYY"))
+        let weatherIcon = $("<img>");
+        weatherIcon.attr("src", iconURL);
+        weatherIcon.attr("alt", iconDescription)
+        let tempEl = $("<p>");
+        tempEl.attr("class", "p-text")
+        
+        let windEl = $("<p>");
+        windEl.attr("class", "p-text")
+        
+        let humidityEl  = $("<p>");
+        humidityEl.attr("class", "p-text")
+        
+        
+        col.append(card)
+        card.append(cardBody)
+        
+        cardBody.append(title, weatherIcon, tempEl, windEl, humidityEl);
+        
+        tempEl.text(`🌡️ ${tempC} ℃`);
+        windEl.text(`🌬️ ${windKph} Kph`);
+        humidityEl.text(`💦 ${humidity} %`);
+        
+        forecastContainer.append(col)
+
+    }
+    
+}
+
+
+// resource for this function: Drew's speedrun
+function fetchWeather(location) {
+    let latitude = location.lat;
+    let longitude = location.lon;
+    let city = location.name;
+    let queryWeatherURL = `${weatherAPIURL}/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${weatherAPIKey}`
+
+    $.ajax({
+        url: queryWeatherURL,
+        method: "GET"
+    }).then(function(response){
+        renderCurrentWeather(city, response.list[0]);
+        renderForecast(response.list);
+
+    })
 }
 
 
@@ -28,11 +150,20 @@ function fetchCoordinates(search) {
             alert("No such location");
         }
         else {
-            searchHistory.push(search);
-            localStorage.setItem("search-history", JSON.stringify(searchHistory))
-            
+            appendSearchHistory(search)
+            fetchWeather(response[0]) 
         }
     })
+}
+
+function initaliseHistory() {
+    let storedHistory = localStorage.getItem("search-history");
+
+    if (storedHistory){
+        searchHistory = JSON.parse(storedHistory);
+    
+    }
+    collectSearchHistory();
 }
 
 
@@ -42,10 +173,21 @@ function submitSearchForm(event) {
     let search = searchInput.val().trim()
 
     fetchCoordinates(search);
+    searchInput.val("");
 }
 
-searchForm.on("submit", submitSearchForm)
+function searchClickHistory(event) {
+    if(!$(event.target).hasClass("btn-history")){
+        return
+    }
+    let search = $(event.target).attr("data-search")
+    fetchCoordinates(search);
+    searchInput.val("")
+}
 
+initaliseHistory()
+searchForm.on("submit", submitSearchForm)
+searchHistoryContainer.on("click", searchClickHistory)
 
 
 
